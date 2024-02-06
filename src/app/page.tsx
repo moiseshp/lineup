2e2f769a9c71c4b41b6356c152fb440ad44cdc99
Moisés Huaringa <moiseseduardo.hp@gmail.com>
2024-02-06 01:34:15 -0500

    add toolbar buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,42 @@ export default function Home() {
       </div>
       <div className="w-[30rem] relative flex flex-col gap-6">
         <div className="border h-[30rem] rounded-md">Board</div>
-        <div className="border">
-          <button>Button</button>
+        <div className="flex gap-4 justify-center">
+          <button className="btn px-8">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+              />
+            </svg>
+            Save
+          </button>
+
+          <button className="btn  btn-primary flex-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+              />
+            </svg>
+            Download
+          </button>
         </div>
         {/* <Toolbar /> */}
       </div>
